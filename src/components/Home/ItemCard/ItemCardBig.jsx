e22b075b1579731f7fef7a3e5c9b5cc3e9dc8e38
EmP33 <purwinmarek16@gmail.com>
2022-03-09 18:18:53 +0100

    Add hover effect for carousel items

--- src/components/Home/ItemCard/ItemCardBig.jsx
+++ src/components/Home/ItemCard/ItemCardBig.jsx
@@ -2,10 +2,27 @@ import React from "react";
 
 import classes from "./ItemCardBig.module.scss";
 
+import {
+  IoCartOutline,
+  IoHeartOutline,
+  IoEllipsisHorizontal,
+} from "react-icons/io5";
+
 const ItemCardBig = ({ name, price, url }) => {
   return (
     <div className={classes.card}>
       <div className={classes["card-container"]}>
+        <div className={classes.cardBackdrop}>
+          <button>
+            <IoCartOutline />
+          </button>
+          <button>
+            <IoHeartOutline />
+          </button>
+          <button>
+            <IoEllipsisHorizontal />
+          </button>
+        </div>
         <img src={url} alt={name} className={classes["card-image"]} />
       </div>
       <div className={classes["card-content"]}>
