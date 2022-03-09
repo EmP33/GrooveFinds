2b452ce5212850a5146602d294d080880d41987c
EmP33 <purwinmarek16@gmail.com>
2022-03-09 15:10:20 +0100

    Create carousel for products

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Menu from "./Menu/Menu";
 import ItemCardBig from "./ItemCard/ItemCardBig";
@@ -7,22 +7,145 @@ import classes from "./Home.module.scss";
 
 import { IoChevronBackOutline, IoChevronForward } from "react-icons/io5";
 
+const bestsItems = [
+  {
+    name: "Computer1",
+    price: "129.95",
+    id: "p1",
+    url: "https://a.allegroimg.com/original/116e82/71826d194940902a6fa56a6884bc/Komputer-do-Gier-Intel-i5-GTX-1050Ti-8GB-Win-10-Kod-producenta-GZi51050Ti",
+  },
+  {
+    name: "Zegarek2",
+    price: "1229.95",
+    id: "p2",
+    url: "https://ustyle.pl/wp-content/uploads/2019/01/1113.jpg",
+  },
+  {
+    name: "Garnek3",
+    price: "59.95",
+    id: "p3",
+    url: "https://florina.pl/pol_pl_Garnek-emaliowany-Emalia-Polska-Pleszew-gladki-bez-pokrywki-16-cm-2-5-l-czerwony-552_1.jpg",
+  },
+  {
+    name: "Computer4",
+    price: "129.95",
+    id: "p4",
+    url: "https://a.allegroimg.com/original/116e82/71826d194940902a6fa56a6884bc/Komputer-do-Gier-Intel-i5-GTX-1050Ti-8GB-Win-10-Kod-producenta-GZi51050Ti",
+  },
+  {
+    name: "Garnek5",
+    price: "59.95",
+    id: "p5",
+    url: "https://florina.pl/pol_pl_Garnek-emaliowany-Emalia-Polska-Pleszew-gladki-bez-pokrywki-16-cm-2-5-l-czerwony-552_1.jpg",
+  },
+  {
+    name: "Zegarek2",
+    price: "1229.95",
+    id: "p6",
+    url: "https://ustyle.pl/wp-content/uploads/2019/01/1113.jpg",
+  },
+  {
+    name: "Computer7",
+    price: "129.95",
+    id: "p7",
+    url: "https://a.allegroimg.com/original/116e82/71826d194940902a6fa56a6884bc/Komputer-do-Gier-Intel-i5-GTX-1050Ti-8GB-Win-10-Kod-producenta-GZi51050Ti",
+  },
+  {
+    name: "Garnek8",
+    price: "59.95",
+    id: "p8",
+    url: "https://florina.pl/pol_pl_Garnek-emaliowany-Emalia-Polska-Pleszew-gladki-bez-pokrywki-16-cm-2-5-l-czerwony-552_1.jpg",
+  },
+  {
+    name: "Computer9",
+    price: "129.95",
+    id: "p9",
+    url: "https://a.allegroimg.com/original/116e82/71826d194940902a6fa56a6884bc/Komputer-do-Gier-Intel-i5-GTX-1050Ti-8GB-Win-10-Kod-producenta-GZi51050Ti",
+  },
+];
+
 const Home = () => {
+  const [divStyle, setDivStyle] = useState({
+    display: "flex",
+    gridGap: "1em",
+    transition: "all 1s ease-in-out",
+    transform: "translateX(-0%)",
+  });
+  const skipSlide = (id) => {
+    if (id === 1) {
+      if (divStyle.transform === "translateX(-66.6666667%)") {
+        setDivStyle({
+          display: "flex",
+          gridGap: "1em",
+          transition: "all 1s ease-in-out",
+          transform: "translateX(-33.333333%)",
+        });
+      } else if (divStyle.transform === "translateX(-33.333333%)") {
+        setDivStyle({
+          display: "flex",
+          gridGap: "1em",
+          transition: "all 1s ease-in-out",
+          transform: "translateX(-0%)",
+        });
+      } else if (divStyle.transform === "translateX(-0%)") {
+        setDivStyle({
+          display: "flex",
+          gridGap: "1em",
+          transition: "all 1s ease-in-out",
+          transform: "translateX(-66.6666667%)",
+        });
+      }
+    } else if (id === 2) {
+      if (divStyle.transform === "translateX(-66.6666667%)") {
+        setDivStyle({
+          display: "flex",
+          gridGap: "1em",
+          transition: "all 1s ease-in-out",
+          transform: "translateX(-0%)",
+        });
+      } else if (divStyle.transform === "translateX(-33.333333%)") {
+        setDivStyle({
+          display: "flex",
+          gridGap: "1em",
+          transition: "all 1s ease-in-out",
+          transform: "translateX(-66.6666667%)",
+        });
+      } else if (divStyle.transform === "translateX(-0%)") {
+        setDivStyle({
+          display: "flex",
+          gridGap: "1em",
+          transition: "all 1s ease-in-out",
+          transform: "translateX(-33.333333%)",
+        });
+      }
+    }
+  };
+
   return (
     <>
       <Menu />
       <div className={classes.wrap}>
         <section className={classes.bests}>
           <h3 className={classes.bestsHeader}>Zobacz nasze Bestsellery</h3>
-          <div className={classes.itemList}>
-            <IoChevronBackOutline className={classes.skipIcon} />
-            <ItemCardBig number={Math.floor(Math.random() * 10)} />
-            <ItemCardBig number={Math.floor(Math.random() * 10)} />
-            <ItemCardBig number={Math.floor(Math.random() * 10)} />
-            {/* <ItemCardBig number={Math.floor(Math.random() * 10)} />
-            <ItemCardBig number={Math.floor(Math.random() * 10)} />
-            <ItemCardBig number={Math.floor(Math.random() * 10)} /> */}
-            <IoChevronForward className={classes.skipIcon} />
+          <div className={classes.center}>
+            <button className={classes.back} onClick={() => skipSlide(1)}>
+              <IoChevronBackOutline />
+            </button>
+            <div className={classes.wrapper}>
+              <div style={divStyle}>
+                {bestsItems.map((item) => (
+                  <ItemCardBig
+                    name={item.name}
+                    price={item.price}
+                    url={item.url}
+                    key={item.id}
+                  />
+                ))}
+              </div>
+            </div>
+            <button className={classes.next} onClick={() => skipSlide(2)}>
+              <IoChevronForward />
+            </button>
           </div>
         </section>
         <section className={classes.deals}>
@@ -32,9 +155,9 @@ const Home = () => {
             <ItemCardBig number={Math.floor(Math.random() * 10)} />
             <ItemCardBig number={Math.floor(Math.random() * 10)} />
             <ItemCardBig number={Math.floor(Math.random() * 10)} />
-            {/* <ItemCardBig number={Math.floor(Math.random() * 10)} />
             <ItemCardBig number={Math.floor(Math.random() * 10)} />
-            <ItemCardBig number={Math.floor(Math.random() * 10)} /> */}
+            <ItemCardBig number={Math.floor(Math.random() * 10)} />
+            <ItemCardBig number={Math.floor(Math.random() * 10)} />
             <IoChevronForward className={classes.skipIcon} />
           </div>
         </section>
--- src/components/Home/ItemCard/ItemCardBig.jsx
+++ src/components/Home/ItemCard/ItemCardBig.jsx
@@ -2,8 +2,18 @@ import React from "react";
 
 import classes from "./ItemCardBig.module.scss";
 
-const ItemCardBig = ({ number }) => {
-  return <div className={classes.card}>ItemCardBig {number}</div>;
+const ItemCardBig = ({ name, price, url }) => {
+  return (
+    <div className={classes.card}>
+      <div className={classes["card-container"]}>
+        <img src={url} alt={name} className={classes["card-image"]} />
+      </div>
+      <div className={classes["card-content"]}>
+        <h3>{name}</h3>
+        <span>{price} zł</span>
+      </div>
+    </div>
+  );
 };
 
 export default ItemCardBig;
